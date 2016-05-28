717b1bcb6bc2de8a5be64d9ce02cc9e369b684a1
杨应海 <yangyinghai@qq.com>
2016-05-29 00:34:05 +0800

    Initial Commit

---
+++ 13-异步加载图像/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  13-异步加载图像
+//
+//  Created by 杨应海 on 16/5/29.
+//  Copyright © 2016年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 13-异步加载图像/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  13-异步加载图像
+//
+//  Created by 杨应海 on 16/5/29.
+//  Copyright © 2016年 itcast. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
