3be82dfa63a556363415aa38875a9e0d0bba71d7
YangYingHai <601503988@qq.com>
2016-05-30 23:42:24 +0800

    在 init 方法中实例化 操作 和缓存

--- 13-异步加载图像/CZWebImage/CZWebImageManager.m
+++ 13-异步加载图像/CZWebImage/CZWebImageManager.m
@@ -8,6 +8,17 @@
 
 #import "CZWebImageManager.h"
 
+@interface CZWebImageManager()
+
+// 图像缓存
+@property (nonatomic, strong) NSMutableDictionary *imageCache;
+// 下载操作缓存
+@property (nonatomic, strong) NSMutableDictionary *operationCache;
+// 下载队列
+@property (nonatomic, strong) NSOperationQueue *downloadQueue;
+
+@end
+
 @implementation CZWebImageManager
 
 + (instancetype)sharedManger {
@@ -19,5 +30,17 @@
     });
     return instance;
 }
+
+// 在 init 方法中初始化队列和缓存
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        _downloadQueue = [[NSOperationQueue alloc] init];
+        _imageCache = [NSMutableDictionary dictionary];
+        _operationCache = [NSMutableDictionary dictionary];
+    }
+    return self;
+}
+
 
 @end
